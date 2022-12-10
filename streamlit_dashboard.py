65216937dee0c324a975e81116daf1e04bc32160
Eliud Rodríguez <43888949+elgiroma@users.noreply.github.com>
2022-12-10 10:53:17 -0700

    Update streamlit_dashboard.py

--- streamlit_dashboard.py
+++ streamlit_dashboard.py
@@ -109,7 +109,7 @@ with col2:
     
     
 st.subheader(
-    "En el municipio de {} hay una correlación del {} entre el presupuesto del estado y la cantidad de alumnos de educación superior matriculados".format(
+    "En el municipio de {} hay una correlación del {}".format(
         chosen_municipality, str(correlation_matrix.iloc[0, -1])
     )
 )
